f5c3974810be44638302d7601d40ff0a368f4874
annashipilo <a.a.shipilo@gmail.com>
2018-03-02 00:57:20 +0300

    calculator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,101 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.initialValue = initialValue;
+    this.actions = [{
+      type: "+",
+      arg: initialValue
+    }];
   }
 
+  valueOf() {
+    var result = 0;
+    
+    for (var i = this.actions.length-1; i > 0 ; i--) {
+      if (this.actions[i].type === "^") {
+        this.actions[i -1].arg = Math.pow(this.actions[i - 1].arg, this.actions[i].arg);
+        result = this.actions[i-1].arg;
+        this.actions.splice(i, 1);
+      }
+    }
+
+    for (var i = 1; i < this.actions.length; i++) {
+      if (this.actions[i].type === "*" || this.actions[i].type === "/") {
+        if (this.actions[i].type === "*") {
+          this.action;
+          var p = this.actions[i - 1].arg;
+          var c = this.actions[i].arg;
+          this.actions[i-1].arg = this.actions[i - 1].arg * this.actions[i].arg;
+          result = this.actions[i-1].arg;
+          this.actions.splice(i, 1);
+          i--;
+        } else {
+          this.actions[i-1].arg = this.actions[i - 1].arg / this.actions[i].arg;
+          result = this.actions[i-1].arg;
+          this.actions.splice(i, 1);
+          i--;
+        }
+      }
+    }
+
+    for (var i = 1; i < this.actions.length; i++) {
+      if (this.actions[i].type === "+" || this.actions[i].type === "-") {
+        if (this.actions[i].type === "+") {
+          this.actions[i - 1].arg = this.actions[i - 1].arg + this.actions[i].arg;
+          result = this.actions[i-1].arg;
+          this.actions.splice(i, 1);
+          i--;
+        } else {
+          this.actions[i - 1].arg = this.actions[i - 1].arg - this.actions[i].arg;
+          result = this.actions[i-1].arg;
+          this.actions.splice(i, 1);
+          i--;
+        }
+      }
+    }
+    return result;
+  }
+  
   add(number) {
-    // your implementation
+    this.actions.push({
+      type: "+",
+      arg: number
+    });
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.actions.push ({
+      type: "-",
+      arg: number
+    }); 
+    return this;
+    
   }
 
   multiply(number) {
-    // your implementation
+    this.actions.push({
+      type: "*",
+      arg: number
+    });
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.actions.push({
+      type: "/",
+      arg: number
+    });
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.actions.push({
+      type: "^",
+      arg: number
+    });
+    return this;
   }
 }
 
 module.exports = SmartCalculator;
+
